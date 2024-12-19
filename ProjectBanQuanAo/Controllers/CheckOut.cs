80ff50bc5ebb9e31f25969c43554487366d7a7a5
BaoAnh524 <baoanh5114@gmail.com>
2024-12-19 23:38:58 +0700

    Cart

---
+++ ProjectBanQuanAo/Controllers/CheckOut.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectBanQuanAo.Controllers
+{
+    public class CheckOut :Controller
+    {
+        
+
+
+        public async Task<IActionResult> Index()
+        { 
+            return View();
+        }
+    }
+}
